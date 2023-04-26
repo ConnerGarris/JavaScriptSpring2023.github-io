9743e1e14f8294fb6927cb10e8d8925d4cd40578
ConnerGarris <conner.garris.22@prcc.edu>
2023-04-25 21:58:07 -0500

    SEPERATED JAVA AND HTML

--- clientside/final/Project2-4/project02-04_txt.js
+++ clientside/final/Project2-4/project02-04_txt.js
@@ -3,8 +3,8 @@
       Project 02-04
 
       Application to calculate the cost of a restaurant order plus tax
-      Author: 
-      Date:   
+      Author: Conner Garris
+      Date: 4/25/2023
 
       Filename: project02-04.js
  */
@@ -12,7 +12,22 @@
 
 
 
-
+function calculateTotal() {
+   var chickenQty = parseInt(document.getElementById('chicken').value);
+   var halibutQty = parseInt(document.getElementById('halibut').value);
+   var burgerQty = parseInt(document.getElementById('burger').value);
+   var salmonQty = parseInt(document.getElementById('salmon').value);
+   var saladQty = parseInt(document.getElementById('salad').value);
+   
+   var foodTotal = chickenQty * 10.95 + halibutQty * 13.95 + burgerQty * 9.95 + salmonQty * 18.95 + saladQty * 7.95;
+   var foodTax = foodTotal * 0.1;
+   var totalBill = foodTotal + foodTax;
+   
+   document.getElementById('foodTotal').innerHTML = '$' + foodTotal.toFixed(2);
+   document.getElementById('foodTax').innerHTML = '$' + foodTax.toFixed(2);
+   document.getElementById('totalBill').innerHTML = '$' + totalBill.toFixed(2);
+   }
+   
 // Function to display a numeric value as a text string in the format $##.## 
  function formatCurrency(value) {
     return "$" + value.toFixed(2);
